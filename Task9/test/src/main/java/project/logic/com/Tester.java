d0798501cf241eb7c3b82d6dd302be8e65fe331f
VadimRomanyuk <47634130+VadimRomanyuk@users.noreply.github.com>
2019-05-20 18:35:23 +0300

    Update Tester

--- Task9/test/src/main/java/project/logic/com/Tester.java
+++ Task9/test/src/main/java/project/logic/com/Tester.java
@@ -1,8 +1,27 @@
 package project.logic.com;
 
-import java.util.Date;
+import project.database.DataBaseHandler;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 
 public class Tester {
     public static void main(String[] args) {
+        try {
+            Connection conn = new DataBaseHandler().getDbConnect();
+            String sql = "UPDATE user SET name = ? WHERE user_id = ?";
+            try(PreparedStatement prSt = conn.prepareStatement(sql))
+            {
+                prSt.setString(1,"Smirnov");
+                prSt.setInt(2,1);
+                prSt.executeUpdate();
+            }
+
+        }
+         catch(Exception ex){
+        System.out.println("Connection failed...");
+        System.out.println(ex);
+    }
     }
 }
