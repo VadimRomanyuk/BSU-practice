c53cf64d87bbe01eed024cbe8b5335e9f5410d24
VadimRomanyuk <47634130+VadimRomanyuk@users.noreply.github.com>
2019-05-20 18:24:52 +0300

    Update 12 task

--- Task9/test/src/main/webapp/resources/js/GlobalFunc.js
+++ Task9/test/src/main/webapp/resources/js/GlobalFunc.js
@@ -109,3 +109,58 @@ let pager = (function(){
      }
     }
 }());
+//                  Tests 
+
+//pager.setPageUser("Petrov");
+/*pager.setPosts( [
+    {id: '1',
+
+    description: 'Great Day',
+
+    createdAt: new Date('2005-02-20T23:00:00'),
+
+    author: 'Ivanov',
+
+    photoLink: 'img/Picture.jpg',
+    
+    likes : ["Petrov","Sidorov"],
+
+    hashTags : ["#sunny"]
+
+   },
+
+   {id: '2',
+
+    description: 'Манчестер Юнайтед в 1/4 финала ЛЧ',
+
+    createdAt: new Date('2016-01-23T23:15:00'),
+
+    author: 'Petrov',
+
+    photoLink: "img/Picture.jpg",
+
+    likes : ["Grealish","Sidorov"],
+
+    hashTags : ["#MU","#WIN","#HardWay","#roadtofinal","#goodluck"]
+
+   },
+
+   {id: '4',
+
+    description: 'Great Player',
+
+    createdAt: new Date('2001-03-12T11:11:00'),
+
+    author: 'Sidorov',
+
+    photoLink: 'img/Picture.jpg',
+
+    likes : ["Petrov","Grealish","Sidorov","Snow"],
+
+    hashTags : ["#color"]
+
+   }]);*/
+//pager.removePost(1);
+//pager.addPost({id: "13",author:"Programmer",description:"Great Day",createdAt:new Date('2005-02-03T23:00:00'),photoLink:"img/Picture.jpg",hashTags:["#COLOR","#RED"],likes:["Petrov"]});
+//pager.editPost({id: "1",author:"Ivanov",description:"RONNEY GOAL",createdAt:new Date('2005-02-20T23:00:00'),photoLink:"img/Ronney.jpg",hashTags:["#sunny"],likes:["Petrov","Sidorov"]});
+//pager.addPost({id: "5",author:"Ivanov",description:"ADD Great Day",createdAt:new Date('2006-02-14'),photoLink:"img/Picture.jpg",hashTags:["#COLOR","#RED"],likes:["Petrov"]});
